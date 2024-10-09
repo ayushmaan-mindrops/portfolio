ce36ba6e460d36ae818a27d45c22d259d2930ab1
AyushmaanRajput <ayushmaanrajput25feb@gmail.com>
2024-10-09 18:26:41 +0530

    Updating deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,18 @@ eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/portfolio
 
 export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" || {
+  echo "NVM is not installed. Installing now...";
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash;
+  . "$NVM_DIR/nvm.sh";
+  nvm install node;
+}
+
+# Check if npm is available
+npm -v || {
+  echo "npm is not installed, installing now...";
+  nvm install node;
+}
 
 git pull origin main
 
