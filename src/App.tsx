01829cc3005ee25a911ce93b3f49e6cd6228f5ff
AyushmaanRajput <ayushmaanrajput25feb@gmail.com>
2024-10-09 18:32:44 +0530

    Testing Ci/Cd

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { toast } from "sonner";
 import "./App.css";
 
 function App() {
   return (
     <>
       <h1>Ayushmaan's Portfolio</h1>
+      <button onClick={() => toast.success("CI/CD words")}>Check Change</button>
     </>
   );
 }
